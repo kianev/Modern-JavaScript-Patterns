c2c0a0271970ae6240bea2d3309d84e2a4f0b07c
Kaloian Ianev <kianev@gmail.com>
2018-02-07 13:16:56 +0100

    Factory Pattern

---
+++ factory.js
@@ -0,0 +1,48 @@
+function MemberFactory () {
+  this.createMember = function (name, type) {
+    let member;
+
+    if(type === 'simple') {
+      member = new SimpleMembership(name)
+    } else if(type === 'standard') {
+      member = new StandardMembership(name)
+    } else if(type === 'super') {
+      member = new SuperMembership(name)
+    }
+
+    member.type = type;
+    
+    member.define = function () {
+      console.log(`${this.name} ${this.type}: ${this.cost}`)
+    }
+
+    return member;
+  }
+}
+
+const SimpleMembership = function (name) {
+  this.name = name;
+  this.cost = '$5';
+}
+
+const StandardMembership = function (name) {
+  this.name = name;
+  this.cost = '$15';
+}
+
+const SuperMembership = function (name) {
+  this.name = name;
+  this.cost = '$25';
+}
+
+const members = [];
+const factory = new MemberFactory();
+
+members.push(factory.createMember('Pesho', 'simple'));
+members.push(factory.createMember('Gosho', 'simple'));
+members.push(factory.createMember('Ivan', 'super'));
+members.push(factory.createMember('Tosho', 'standard'));
+
+members.forEach(member => {
+  member.define();
+})
